067be5c96ad09e75cca4c1a334add23fbc5c3f8c
shatanyumi <1143005768@qq.com>
2024-02-05 15:08:13 +0800

    src file add ...

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+# This is the __init__.py file for the SAVD package.
+
+# Import any modules or subpackages you want to make available when importing the package.
+# For example:
+# from . import module1
+# from . import module2
+# from . import subpackage1
+# from . import subpackage2
+
+# You can also define any initialization code or variables here.
+
+# Remember to include the necessary comments and documentation to make your code clear and understandable.
