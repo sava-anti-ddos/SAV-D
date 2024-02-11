626d96757ca7cad0ac2d2acd3f9b1e9121b698b7
shatanyumi <1143005768@qq.com>
2024-02-11 11:18:32 +0800

    rm redict...

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,6 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-master_doc = 'index_redirect'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
